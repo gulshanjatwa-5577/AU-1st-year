bda2b740efbeed9eda093a0680d1c2782b8e1125
gulshanjatwa-5577 <gulshan.jatwa@avantika.edu.in>
2026-01-30 21:23:22 +0530

    add namespace support for project

---
+++ .vscode/class_work/namespace/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ namespace G1{
+    void scr(){
+        cout<<"Inside G1 scr"<<endl;
+    }
+ }
+    namespace G2{
+        void scr(){
+            cout<<"Inside G2 scr"<<endl;
+        }
+    }
+    int main(){
+        G1::scr();
+        G2::scr();
+    }
