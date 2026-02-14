6fa0d91d63d70e81c7ee6f2b0e92c25fc3fa4098
gulshanjatwa-5577 <gulshan.jatwa@avantika.edu.in>
2026-02-14 10:13:55 +0530

    size of data types in bytes

---
+++ .vscode/youtubprogram/sizeof.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"size of int is "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"size of char is "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"size of float is "<<sizeof(float)<<" bytes"<<endl;
+    return 0;
+}
